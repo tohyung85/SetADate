700a12876e0e781145126d379c739242d08568ad
Joshua <tantohyung@gmail.com>
2015-10-31 17:52:40 +0800

    Set up Individual Group Screen Table

---
+++ SetADate/IndividualGroupScreenCell.swift
@@ -0,0 +1,18 @@
+//
+//  IndividualGroupScreenCell.swift
+//  SetADate
+//
+//  Created by Joshua  Tan on 31/10/15.
+//  Copyright © 2015 Joshua. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class IndividualGroupScreenCell : UITableViewCell {
+    
+    @IBOutlet weak var eventLocationLabel: UILabel!
+    @IBOutlet weak var eventTimeLabel: UILabel!
+    @IBOutlet weak var eventStartDateLabel: UILabel!
+    @IBOutlet weak var eventNameLabel: UILabel!
+}
--- SetADate/IndividualGroupViewController.swift
+++ SetADate/IndividualGroupViewController.swift
@@ -10,14 +10,21 @@ import Foundation
 import UIKit
 import Contacts
 
-class IndividualGroupViewController : UIViewController {
+class IndividualGroupViewController : UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var group : Group?
     var detailsVC : UIViewController?
+    var calendar : NSCalendar?
+    @IBOutlet weak var individualGroupTable: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.calendar = NSCalendar.currentCalendar()
+        
+        self.individualGroupTable.dataSource = self
+        self.individualGroupTable.delegate = self
+        
         let frame = UIScreen.mainScreen().bounds
 
         let titleView = TitleSubtitleView(frame: CGRectMake(0, 0, frame.width / 3, 50), titleText: (self.group?.name)!, subtitleText: "Tap for more info")
@@ -44,4 +51,22 @@ class IndividualGroupViewController : UIViewController {
     func titleViewTapped (sender: TitleSubtitleView) {
         self.navigationController?.pushViewController(self.detailsVC!, animated: true)
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (self.group?.events?.count)!
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellIndentifier = "individualGroupScreenCell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIndentifier) as! IndividualGroupScreenCell
+        let event = self.group?.events![indexPath.row]
+        cell.eventNameLabel.text = event?.eventTitle
+        cell.eventStartDateLabel.text = event?.eventStartDate?.dateString(self.calendar!)
+        let timeText = (event?.eventStartDate?.timeString(self.calendar!))! + " - " + (event?.eventEndDate?.timeString(self.calendar!))!
+        cell.eventTimeLabel.text = timeText
+        cell.eventLocationLabel.text = event?.eventLocation
+        
+        return cell
+    }
+    
 }
